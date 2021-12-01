ff00e3e9e746f5870e7a1260ae5ded062a01981b
ZaeroX <ferruda@gmail.com>
2021-12-01 19:16:44 -0300

    count cambiado

--- main.tf
+++ main.tf
@@ -45,10 +45,10 @@ resource "azurerm_resource_group" "tf_jenkins" {
 # }
 
 resource "azurerm_app_service_plan" "svcplan" {
-  for_each = {for rg in  azurerm_resource_group.tf_jenkins: rg.name => rg}
+  count    = var.resource_group_count
   name                = "serviceplantfjenkins"
-  location            = "azurerm_resource_group.tf_jenkins${each.key}.location"
-  resource_group_name = "azurerm_resource_group.tf_jenkins${each.key}.name"
+  location            = azurerm_resource_group.tf_jenkins[count.index].location
+  resource_group_name = azurerm_resource_group.tf_jenkins[count.index].name
 
   sku {
     tier = "Standard"
@@ -57,11 +57,11 @@ resource "azurerm_app_service_plan" "svcplan" {
 }
 
 resource "azurerm_app_service" "appsvc" {
-  for_each = {for rg in  azurerm_resource_group.tf_jenkins: rg.name => rg}
+  count    = var.resource_group_count
   name                = "appservicetfjenkins"
-  location            = "azurerm_resource_group.tf_jenkins${each.key}.location"
-  resource_group_name = "azurerm_resource_group.tf_jenkins${each.key}.name"
-  app_service_plan_id = "azurerm_app_service_plan.svcplan${each.key}.id"
+  location            = azurerm_resource_group.tf_jenkins[count.index].location
+  resource_group_name = azurerm_resource_group.tf_jenkins[count.index].name
+  app_service_plan_id = azurerm_app_service_plan.svcplan[count.index].id
 
 
   site_config {
