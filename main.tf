3a9c960c9d467dc1a1d169a4c6a59817406cf4ad
ZaeroX <ferruda@gmail.com>
2021-12-01 15:45:18 -0300

    cambiado a jenkis

--- main.tf
+++ main.tf
@@ -41,8 +41,8 @@ resource "azurerm_resource_group" "tf_jenkins" {
 
 resource "azurerm_app_service_plan" "svcplan" {
   name                = "serviceplantfjenkins"
-  location            = azurerm_resource_group.tf_test.location
-  resource_group_name = azurerm_resource_group.tf_test.name
+  location            = azurerm_resource_group.tf_jenkins.location
+  resource_group_name = azurerm_resource_group.tf_jenkins.name
 
   sku {
     tier = "Standard"
@@ -52,8 +52,8 @@ resource "azurerm_app_service_plan" "svcplan" {
 
 resource "azurerm_app_service" "appsvc" {
   name                = "appservicetfjenkins"
-  location            = azurerm_resource_group.tf_test.location
-  resource_group_name = azurerm_resource_group.tf_test.name
+  location            = azurerm_resource_group.tf_jenkins.location
+  resource_group_name = azurerm_resource_group.tf_jenkins.name
   app_service_plan_id = azurerm_app_service_plan.svcplan.id
 
 
