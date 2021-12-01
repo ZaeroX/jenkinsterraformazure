f9a2838a97492a870bbf781707157eff3d82d2be
ZaeroX <ferruda@gmail.com>
2021-12-01 15:34:00 -0300

    asdasd

---
+++ main.tf
@@ -0,0 +1,68 @@
+#configurethe azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+# provider "azurerm" {
+#   features {}
+#   skip_provider_registration = true
+# }
+
+resource "azurerm_resource_group" "tf_test" {
+  name     = "tfacloudguru"
+  location = "Central US"
+}
+
+# #create a virtual network
+# resource "azurerm_virtual_network" "vnet" {
+#   name                = "BatmanInc"
+#   address_space       = ["10.0.0.0/16"]
+#   location            = azurerm_resource_group.tf_test.location
+#   resource_group_name = azurerm_resource_group.tf_test.name
+#   tags = {
+#     Environment = "TheBatCave"
+#     Team        = "Batman"
+#   }
+# }
+
+# #create subnet
+# resource "azurerm_subnet" "subnet" {
+#   name                 = var.azurerm_subnet_name
+#   resource_group_name  = azurerm_resource_group.tf_test.name
+#   virtual_network_name = azurerm_virtual_network.vnet.name
+#   address_prefix       = "10.0.1.0/24"
+# }
+
+resource "azurerm_app_service_plan" "svcplan" {
+  name                = "Enter App Service Plan name"
+  location            = azurerm_resource_group.tf_test.location
+  resource_group_name = azurerm_resource_group.tf_test.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "appsvc" {
+  name                = "Enter Web App Service Name"
+  location            = azurerm_resource_group.tf_test.location
+  resource_group_name = azurerm_resource_group.tf_test.name
+  app_service_plan_id = azurerm_app_service_plan.svcplan.id
+
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+}
+
+
+
+
